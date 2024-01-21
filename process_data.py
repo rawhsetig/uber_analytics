b616327dd1757af5e82872f3070383e4d8d2128f
rawhsetig <giteshwarmali@gmail.com>
2024-01-21 15:47:04 +0530

    filtering methods on dataframe

--- process_data.py
+++ process_data.py
@@ -1,12 +1,12 @@
 import pandas as pd 
 from geopy.geocoders import Nominatim
 
-file = pd.read_csv("./uber_data.csv")
-# print(file.head)
-# for col in file.columns:
-    # print(col)
+data = pd.read_csv("./uber_data.csv")
+# print(data.head)
+# for col in data.columns:
+#     print(col)
 sum = 0.0
-for val in file['total_amount']:
+for val in data['total_amount']:
     sum += val
 print("total revenue : ", sum) #$1639072.0900016439
 #75.057487 lat : 21.035400
@@ -17,5 +17,21 @@ geolocator = Nominatim(user_agent="geoapiExercises")
 lon = 75.057487 
 lat = 21.035400
 
-location = geolocator.geocode(str(lat)+","+ str(lon))
-print(location)
+# location = geolocator.geocode(str(lat)+","+ str(lon))
+# print(location)
+
+#Filter non null tpep_pickup and dropoff times
+# df = data[data['tpep_pickup_datetime'] != 0]
+
+#Filter using query function
+# df = data.query("total_amount == 12.35")
+# print(df)
+
+#Filter using lambda
+# print([df['VendorID'].isin([2])])
+df = data.apply(
+    lambda row: row[data['tpep_pickup_datetime'].isin([0])]
+)
+print(df)
+
+# df = pd.DataFrame()
